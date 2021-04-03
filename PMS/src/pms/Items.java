17cdadf432916e88c960e802fea8db550f74ec7e
faraz7321 <81792548+faraz7321@users.noreply.github.com>
2021-04-03 22:14:58 +0500

    login forms created

---
+++ PMS/src/pms/Items.java
@@ -0,0 +1,70 @@
+package pms;
+
+/**
+ *
+ * @author Faraz Ahmad
+ */
+public class Items {
+
+    private String itemName;
+    private String category;
+    private int price;
+    private int quantity;
+    private boolean isConsumable;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean isIsConsumable() {
+        return isConsumable;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setIsConsumable(boolean isConsumable) {
+        this.isConsumable = isConsumable;
+    }
+
+    public boolean addItem() {
+        boolean flag = false;
+        return flag;
+    }
+
+    public boolean deleteItem() {
+        boolean flag = false;
+        return flag;
+    }
+
+    public boolean updateItem() {
+        boolean flag = false;
+        return flag;
+    }
+
+}
--- PMS/src/pms/PMS.java
+++ PMS/src/pms/PMS.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pms;
 
 /**
@@ -16,6 +11,8 @@ public class PMS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Login login = new Login();
+        login.setVisible(true);
     }
 
 }
